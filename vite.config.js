b50da1207a347657cac6d5f905bb14510d145f89
StarJoy Dev <dev@starsjoy.com>
2026-03-16 03:38:39 +0500

    update

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,12 @@ export default defineConfig({
   server: {
     port: 5173,
     open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      }
+    }
   },
   build: {
     outDir: 'dist',
